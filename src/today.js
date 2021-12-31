f0e6f8f1cb143d0207779927e721cff1bcf8aeeb
Cooper Adams <cooperadamsbusiness@gmail.com>
2021-12-30 21:41:44 -0500

    Added bare logic to display the todo items in the Today category

--- src/today.js
+++ src/today.js
@@ -7,6 +7,19 @@ function loadToday(todaysItems)
         todoContent.textContent = "There's nothing here! Click the plus symbol in the menu to add a new item.";
     }
 
+    else
+    {
+        //First, clear todoContent
+        while (todoContent.firstChild)
+        {
+            todoContent.removeChild(firstChild);
+        }
+
+        for (let i = 0; i < todaysItems.length(); ++i)
+        {
+            const displayItem = document.createElement('div');
+        }
+    }
 }
 
 export default loadToday;
