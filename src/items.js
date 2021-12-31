197672cddbb8f4abbfa49de316452ff3d412f0e0
Cooper Adams <cooperadamsbusiness@gmail.com>
2021-12-30 21:38:52 -0500

    Removed unnecessary projectFactory

--- src/items.js
+++ src/items.js
@@ -3,11 +3,6 @@ export function todoItemFactory(title, description, dueDate, priority)
     return { title: title, description: description, dueDate: dueDate, priority: priority };
 }
 
-/*export const projectFactory = (title) =>
-{
-    return { title: title }
-}*/
-
 export const noteFactory = (header, description) =>
 {
     return { header: header, description: description };
