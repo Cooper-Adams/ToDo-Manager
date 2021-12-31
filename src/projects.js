baba3a0b9631688930ae5b0b140be235863937ad
Cooper Adams <cooperadamsbusiness@gmail.com>
2021-12-30 21:40:37 -0500

    Displays the submenu when the Projects option is clicked

--- src/projects.js
+++ src/projects.js
@@ -1,11 +1,10 @@
-function loadProjects(projectList)
+function loadProjects()
 {
     const todoContent = document.querySelector('.todoContent');
 
-    if (projectList.length == 0)
-    {
-        todoContent.textContent = "There's nothing here! Click the plus symbol in the menu to add a new project.";
-    }
+    const subMenuOptions = document.querySelectorAll('.projectOption');
+    subMenuOptions.forEach(option =>
+        option.style.display = 'block');
 }
 
 export default loadProjects;
