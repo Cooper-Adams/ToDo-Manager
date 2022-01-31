1f95d296e5299ddf9c7b690c7fbfea5d3835523f
Cooper Adams <cooperadamsbusiness@gmail.com>
2022-01-30 23:48:20 -0500

    Updated the todoItemFactory function to give a project data member. Also defined functions.

--- src/items.js
+++ src/items.js
@@ -1,8 +1,25 @@
-export function todoItemFactory(title, description, dueDate, priority)
+/**
+ * Factory for todo creation
+ * 
+ * @param {*} title The title of the todo
+ * @param {*} description The details of the todo
+ * @param {*} dueDate The date of the todo
+ * @param {*} priority The priority of the todo
+ * @param {*} project The project the todo belongs to
+ * @returns object
+ */
+export function todoItemFactory(title, description, dueDate, priority, project)
 {
-    return { title: title, description: description, dueDate: dueDate, priority: priority };
+    return { title: title, description: description, dueDate: dueDate, priority: priority, project: project };
 }
 
+/**
+ * Factory for note creation
+ * 
+ * @param {*} header The title of the note
+ * @param {*} description The details of the note
+ * @returns object
+ */
 export function noteFactory(header, description)
 {
     return { header: header, description: description };
